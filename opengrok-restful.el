3bd859a1d8f7c17e3b52471c8f606c70b709af85
Bingyu Zhou <rain.by.zhou@gmail.com>
2020-11-16 11:20:43 +0800

    first commit
    Signed-off-by: bingyu.zby <bingyu.zby@alibaba-inc.com>

---
+++ opengrok-restful.el
@@ -0,0 +1,34 @@
+;;; opengrok-restful.el --- Emacs Source Code Search With OpenGrok RESTful API -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020 RainMark
+
+;; Author: RainMark <rain.by.zhou at gmail.com>
+;; URL: https://github.com/RainMark/opengrok-restful.el
+;; Package-Requires: ((emacs "24.4"))
+;; Version: 0.0.1
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; opengrok restful interface for Emacs
+;;
+;; See documentation on https://github.com/RainMark/opengrok-restful.el
+
+;;; Code:
+
+
+;;; opengrok-restful.el ends here
